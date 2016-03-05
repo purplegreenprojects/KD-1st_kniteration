716ce31d41554be4b9ff18b04f52ad8ab64e1a48
eef22 <eford1@binghamton.edu>
2016-03-04 21:38:56 -0500

    in case i really screw up step 3 checkboxes

--- KDstep3pattern.php
+++ KDstep3pattern.php
@@ -17,7 +17,7 @@
 		}
 		elseif($_GET["texture"]=="comfy") {
 		$arrayyarncolors = $arraycomfycolors;
-	}
+		}
 	}
 	
 	if(isset ($arrayyarncolors)){
@@ -28,6 +28,7 @@
 			if($color[0] == $_GET["cc"]) {
 				$ccurl = $color[1];
 			}
+		}
 		foreach ($arrayyarncolors as $color) {
 			if(isset($_POST[$color[0]])) {
 				header ("location: KDstep3pattern.php?texture=".$_GET["texture"]."&maincolor=".$_GET["maincolor"]. "&cc=".$color[0]);
@@ -37,60 +38,65 @@
 ?>
 
 <html>
-<head>
-	<title> Kniterative Designs </title>
-	<link rel='shortcut icon' href='kniterative designs logo 1000.png'>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Pacifico:regular,bold;italic;bolditalic">
-	<link rel='stylesheet' href="stylesheet.css">
-</head>
+	<head>
+		<title> Kniterative Designs </title>
+		<link rel='shortcut icon' href='kniterative designs logo 1000.png'>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Pacifico:regular,bold;italic;bolditalic">
+		<link rel='stylesheet' href="stylesheet.css">
+	</head>
 
-<body>
-	<div style='font-size:42px'> <b> Step 3: Coat Pattern </b> </div>
-	<div style= 'font-size:28px'> Choose Contrasting Color Areas </div> 
-<hr>
+	<body>
+		<div style='font-size:42px'> <b> Step 3: Coat Pattern </b> </div>
+		<div style= 'font-size:28px'> Choose Contrasting Color Areas </div> 
+		<hr>
 
-<form method='post'>
+		<form method='post'>
+		<br>
+		
+		<?php
 
+			$hidden = "hidden";
 
+			if (isset ($_POST["cclbp_hide"])) {
+				$cclbp_hide = "";
+			}
+			else {
+				$cclbp_hide = "hidden";
+			}
 
-
-	$hidden = "hidden";
+			if (isset ($_POST["ccrbp_hide"])) {
+				$ccrbp_hide = "";
+			}
+			else {
+				$ccrbp_hide = "hidden";
+			}
 
 // \/ will take out once i have the real checkboxes - for now, it's there so i don't get the "undefined variable" error message
-	$cclbp_hide = "hidden";
-	$ccrbp_hide = "";
+	
 	$cclfp_hide = "";
 	$ccrfp_hide = "";
 	$ccstomachchin_hide = "";
 	$cctail_hide = "";
 // (^ take out)
 
-	echo "<div style='display:inline-block; height:250px; width:250px; background-color:grey'>
-			<div style='position:absolute; background-color:white' id='cookiecuttercatface' class:'catpart'>
-			</div>
-		</div>
+			echo "<div style='display:inline-block; height:250px; width:250px; background-color:grey'>
+					<div style='position:absolute; background-color:white' id='cookiecuttercatface' class:'catpart'>
+					</div>
+				</div>
 
-		<div style= 'float: right; height: 500px; width: 500px; background-color:grey'>
-			<div style='position: absolute; background-image: url(".$maincolorurl.");' id='cookiecuttercatprofile' class='catpart'> </div>
-			<div style= 'background-image: url(".$ccurl.");' id= 'cclbp' class='".$cclbp_hide." catpart'></div>
-			<div style= 'background-image: url(".$ccurl.");' id= 'ccrbp' class='".$ccrbp_hide." catpart'></div>
-			<div style= 'background-image: url(".$ccurl.");' id= 'cclfp' class='".$cclfp_hide." catpart'></div>
-			<div style= 'background-image: url(".$ccurl.");' id= 'ccrfp' class='".$ccrfp_hide." catpart'></div>
-			<div style= 'background-image: url(".$ccurl.");' id= 'ccstomach' class='".$ccstomachchin_hide." catpart'></div>
-			<div style= 'background-image: url(".$ccurl.");' id= 'ccchest' class='".$ccstomachchin_hide." catpart'></div>
-			<div style= 'background-image: url(".$ccurl.");' id= 'ccchin' class='".$ccstomachchin_hide." catpart'></div>
-			<div style= 'background-image: url(".$ccurl.");' id= 'cctail' class='".$cctail_hide." catpart'></div>
-		</div>";
-
-if (isset ($_POST["cclbp_hide"])) {
-$cclbp_hide = "";
-}
-else {
-$cclbp_hide = "hidden";
-}
-
-?>
+				<div style= 'float: right; height: 500px; width: 500px; background-color:grey'>
+					<div style='position: absolute; background-image: url(".$maincolorurl.");' id='cookiecuttercatprofile' class='catpart'> </div>
+					<div style= 'background-image: url(".$ccurl.");' id= 'cclbp' class='".$cclbp_hide." catpart'></div>
+					<div style= 'background-image: url(".$ccurl.");' id= 'ccrbp' class='".$ccrbp_hide." catpart'></div>
+					<div style= 'background-image: url(".$ccurl.");' id= 'cclfp' class='".$cclfp_hide." catpart'></div>
+					<div style= 'background-image: url(".$ccurl.");' id= 'ccrfp' class='".$ccrfp_hide." catpart'></div>
+					<div style= 'background-image: url(".$ccurl.");' id= 'ccstomach' class='".$ccstomachchin_hide." catpart'></div>
+					<div style= 'background-image: url(".$ccurl.");' id= 'ccchest' class='".$ccstomachchin_hide." catpart'></div>
+					<div style= 'background-image: url(".$ccurl.");' id= 'ccchin' class='".$ccstomachchin_hide." catpart'></div>
+					<div style= 'background-image: url(".$ccurl.");' id= 'cctail' class='".$cctail_hide." catpart'></div>
+				</div>";
+		?>
 
 <br><br>
 
@@ -116,19 +122,7 @@ $cclbp_hide = "hidden";
 
 </div>
 
-<?php
 
-//Checkboxes action
-
-if (isset ($_POST["cclbp_hide"])) {
-$cclbp_hide = "";
-}
-else {
-$cclbp_hide = "hidden";
-}
-
-
-?>
 </form>
 </body>
 </html>
--- KDstep3patternTEST.php
+++ KDstep3patternTEST.php
@@ -1,44 +1,37 @@
 <html>
-<head>
-	<title> Kniterative Designs </title>
-	<link rel='shortcut icon' href='kniterative designs logo 1000.png'>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Pacifico:regular,bold;italic;bolditalic">
-	<link rel='stylesheet' href="stylesheet.css">
-</head>
+	<head>
+		<title> Kniterative Designs </title>
+		<link rel='shortcut icon' href='kniterative designs logo 1000.png'>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Pacifico:regular,bold;italic;bolditalic">
+		<link rel='stylesheet' href="stylesheet.css">
+	</head>
 
-<body>
-	<div style='font-size:42px'> <b> Step 3: Coat Pattern </b> </div>
-	<div style= 'font-size:28px'> Choose Contrasting Color Areas </div> 
-<hr>
+	<body>
+		<div style='font-size:42px'> <b> Step 3: Coat Pattern </b> </div>
+		<div style= 'font-size:28px'> Choose Contrasting Color Areas </div> 
+	<hr>
 
-<div style='display:inline-block; height:250px; width:250px; background-color:grey'>
+	<form method='post'>
+
+		<div style='display:inline-block; height:250px; width:250px; background-color:grey'>
 			<div style='position:absolute; background-color:white;' id='cookiecuttercatface' class:'catpart'>
 			</div>
 		</div>
 
-<div style= 'height:200px; width:250px; background-color:yellow; float:left'>
-	<div style= 'background-image: url(https://d2d00szk9na1qq.cloudfront.net/Product/2af5de9f-faca-4854-90f3-bb3e79ab04bc/Images/Medium_LBY-253.jpg)' id='cookiecuttercatface' class='catpart'></div>
+		<div style= 'float: right; height: 500px; width: 500px; background-color:grey'>
+			<div style= 'background-image: url(https://d2d00szk9na1qq.cloudfront.net/Product/2af5de9f-faca-4854-90f3-bb3e79ab04bc/Images/Medium_LBY-253.jpg)' id='cookiecuttercatprofile' class='catpart'></div>
+			<div style= 'background-color:green' id= 'ccchest' class='catpart'></div>
+			<div style= 'background-color:blue' id= 'ccchin' class='catpart'></div>
+			<div style= 'background-color:red' id= 'cctail' class='catpart'></div>
+		</div>
 
+		<div id='checkboxesForm' style='line-height: 200%'>
+			<input type='checkbox' name='cclbp'> Back Paw (Left)
+			<br>
+			<input type='checkbox' name='ccrbp'> Back Paw (Right)
+		</div>
 
-
-
-</div>
-<div id='checkboxesForm' style='line-height: 200%'>
-	<input type='checkbox' name='cclbp'> Back Paw (Left)
-	<br>
-	<input type='checkbox' name='ccrbp'> Back Paw (Right)
-</div>
-
-
-
-<div style= 'float: right; height: 500px; width: 500px; background-color:grey'>
-	<div style= 'background-image: url(https://d2d00szk9na1qq.cloudfront.net/Product/2af5de9f-faca-4854-90f3-bb3e79ab04bc/Images/Medium_LBY-253.jpg)' id='cookiecuttercatprofile' class='catpart'></div>
-	<div style= 'background-color:green' id= 'ccchest' class='catpart'></div>
-	<div style= 'background-color:blue' id= 'ccchin' class='catpart'></div>
-	<div style= 'background-color:red' id= 'cctail' class='catpart'></div>
-</div>
-
-
+</form>
 </body>
 </html>
