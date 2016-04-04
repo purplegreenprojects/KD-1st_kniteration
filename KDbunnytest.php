f28c741986c00fb312ece5db0551597847b234ed
eef22 <eford1@binghamton.edu>
2016-04-03 20:50:41 -0400

    bunny clip-paths, clip-path tools, profile div gradients

--- KDbunnytest.php
+++ KDbunnytest.php
@@ -6,8 +6,24 @@
 </head>
 <body>
 
-<div style='background-color: gray; height: 500px; width:500px'>
-	<div class='bunnyprofile' style='background-color:blue'> </div>
+
+<div style='position: absolute; background-color: gray; height: 500px; width:500px'>
+	<div class='bunnypart' id='bunnyprofile' style='background-image: url(https://d2d00szk9na1qq.cloudfront.net/Product/4e37b5dc-971b-4ccc-957a-23de0ad48c60/Images/Large_LBY-246.jpg)'> </div>
+	<!-- <div class='catpart' id='bunnybacklegOFF' style='background-color:orange'> </div> -->
+	<div class='bunnypart' id='bunnybacklegdivider' style='background-color:black'></div>
+	<div class='bunnybackleg' id='bunnybackleg' style='background-image: url(https://d2d00szk9na1qq.cloudfront.net/Product/4e37b5dc-971b-4ccc-957a-23de0ad48c60/Images/Large_LBY-246.jpg)'> </div>
+
+	<div class='bunnypart' id='bunnyprofile_wholetail' style='background-image: url(https://d2d00szk9na1qq.cloudfront.net/Product/ab5b3190-3022-44db-8a1a-189a8d4e8ec7/Images/Large_LBY-269.jpg)'> </div>
+
+	<div class='bunnypart' id='bunnyprofileearback' style='background-image: url(https://d2d00szk9na1qq.cloudfront.net/Product/4e37b5dc-971b-4ccc-957a-23de0ad48c60/Images/Large_LBY-246.jpg)'> </div>
+	<div class='bunnypart' id='bunnyeardivider' style='background-color:black'></div>
+	<div class='bunnypart' id='bunnyprofileearfront' style='background-image: url(https://d2d00szk9na1qq.cloudfront.net/Product/4e37b5dc-971b-4ccc-957a-23de0ad48c60/Images/Large_LBY-246.jpg)'> </div>
+	<div class='bunnypart' id='bunnyprofileinnerear' style='background-image: url(https://d2d00szk9na1qq.cloudfront.net/Product/ab5b3190-3022-44db-8a1a-189a8d4e8ec7/Images/Large_LBY-269.jpg)'> </div>
+	<div class='bunnypart' id='bunnyprofile_eye' style='background-color:black'> </div>
+	<div class='bunnypart' id='bunnyprofile_nose' style='background-color:pink'> </div>
+
+	<div class='bunnypart' id='bunnyprofile_chin' style='background-image: url(https://d2d00szk9na1qq.cloudfront.net/Product/ab5b3190-3022-44db-8a1a-189a8d4e8ec7/Images/Large_LBY-269.jpg)'> </div>
+	<div class='bunnypart' id='bunnyprofile_stomach' style='background-image: url(https://d2d00szk9na1qq.cloudfront.net/Product/ab5b3190-3022-44db-8a1a-189a8d4e8ec7/Images/Large_LBY-269.jpg)'> </div>
 </div>
 
 </body>
